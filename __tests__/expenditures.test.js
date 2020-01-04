0ca0f903c963752adf8db81f481ae84bb0b5ddfd
Ian Andrewson <ianandrewson@gmail.com>
2020-01-04 15:16:47 -0800

    test written for expenditure post route, ready to TDD that single route

---
+++ __tests__/expenditures.test.js
@@ -0,0 +1,65 @@
+require('dotenv').config();
+const request = require('supertest');
+const app = require('../lib/app.js');
+const User = require('../lib/models/User.js');
+const Budget = require('../lib/models/Budget.js');
+const Expenditure = require('../lib/models/Expenditure.js');
+const mongoose = require('mongoose');
+const connect = require('../lib/utils/connect.js');
+
+describe('expenditure route tests', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  let agent;
+  let user;
+  let budget;
+  beforeEach(async() => {
+    agent = request.agent(app);
+
+    user = await User.create({
+      email: 'test@test.test',
+      password: 'password'
+    });
+
+    await agent
+      .post('/api/v1/auth/login')
+      .send({ email: 'test@test.test', password: 'password' });
+
+    budget = await Budget.create({
+      userId: user.id,
+      monthOf: '1/2020',
+      budget: 1000
+    });
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('should be able to post a new expenditure', async() => {
+    await agent
+      .post('/api/v1/expenditures')
+      .send({
+        budgetId: budget._id,
+        item: 'groceries',
+        cost: 43.22,
+        dateOfExpenditure: new Date('1/4/2020')
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          budgetId: budget._id,
+          item: 'groceries',
+          cost: 43.22,
+          dateOfExpenditure: ('1/4/2020'),
+          __v: 0
+        });
+      });
+  });
+});
